ed566dde4381b34d1f02a1e446e9256e45916e2a
Mohamed Wael <mohamedabd-elaziz@outlook.com>
2024-06-15 23:33:31 +0300

    Update mgpt.py

--- mgpt.py
+++ mgpt.py
@@ -42,7 +42,7 @@ class MotionGPT(BaseModel):
 
         # Instantiate motion-language model
         print("HEREEEEEEEEEEEEEE ",lm)
-        print("TYZZZZZZZZZZZZZ: ",cfg.model.lm)
+        print("TYZZZZZZZZZZZZZ: ",cfg.model.params.lm)
         self.lm = instantiate_from_config(lm)
 
         # Freeze the motion tokenizer for lm training
