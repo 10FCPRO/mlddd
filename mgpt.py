f543382a362d4ebb43051fbcdbb69356095b56dd
Mohamed Wael <mohamedabd-elaziz@outlook.com>
2024-06-22 00:27:54 +0300

    Update mgpt.py

--- mgpt.py
+++ mgpt.py
@@ -71,7 +71,8 @@ class MotionGPT(BaseModel):
         # Forward
         # texts = ['Generate motion: ' + text for text in texts]
         outputs, output_texts = self.lm.generate_direct(texts, do_sample=True)
-        print("\nOutputs: ",outputs.size())
+        print("\nOutputs: ",len(outputs))
+        print("outputs: ",outputs[i].size())
         # Motion Decode
         feats_rst_lst = []
         lengths = []
