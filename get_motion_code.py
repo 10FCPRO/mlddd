ea40ddb101f92ed915dfe00c9d986a43ad399776
Mohamed Wael <mohamedabd-elaziz@outlook.com>
2024-06-27 22:30:21 +0300

    Update get_motion_code.py

--- get_motion_code.py
+++ get_motion_code.py
@@ -34,6 +34,7 @@ def main():
     print(cfg)
     print("O)O)O)O)O)O)O)O)O)O)O)")
     print(cfg.lm.default.params.ablation)
+    print("kokokokoko")
     print(cfg.model.params.motion_vae)
     #os.makedirs(output_dir, exist_ok=True)
 
